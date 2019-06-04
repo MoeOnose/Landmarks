801f0d97614e469c0a7c56438f1f4dbaa3581301
Moe Onose <onose.moe.infratop@gmail.com>
2019-06-05 04:42:36 +0900

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 小野瀬萌 on 2019/06/05.
+//  Copyright © 2019 小野瀬萌. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
